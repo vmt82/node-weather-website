72ea894463ab28560a3311414ddc97712e3162b4
Valentin-Marian Tudor <valentin.tudor11@gmail.com>
2022-02-22 17:58:38 +0200

    Added extra info in forecast.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -7,8 +7,8 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to connect to weather service', undefined)
         } else if (body.error) {
             callback('Unable to find location', undefined)
-        } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.')
+        } else {           
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. Humidity is ' + body.current.humidity + '%.')
         }
     })
 }
